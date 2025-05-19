fc7f357888fc9cc8b20c94cba157dc7fd02dfba3
Foglas <kubik.fogl@gmail.com>
2025-05-19 21:38:58 +0200

    docker network added

--- src/main/kotlin/com/fim/prototype/mish/controllers/ChapterController.kt
+++ src/main/kotlin/com/fim/prototype/mish/controllers/ChapterController.kt
@@ -2,13 +2,17 @@ package com.fim.prototype.mish.controllers
 
 import com.fim.prototype.mish.data.ChapterEntity
 import com.fim.prototype.mish.services.ChapterService
-import com.mongodb.client.gridfs.model.GridFSFile
 import org.springframework.core.io.InputStreamResource
-import org.springframework.data.mongodb.gridfs.GridFsResource
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import java.io.InputStream
+
 
 @RestController
 @RequestMapping("/api/chapter")
