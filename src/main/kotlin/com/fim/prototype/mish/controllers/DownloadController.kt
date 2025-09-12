aca8ac096cccc6eece9d4fd85342e3c6c81e8cf9
Foglas <kubik.fogl@gmail.com>
2025-09-12 13:42:34 +0200

    cross origin + updated docker compose

--- src/main/kotlin/com/fim/prototype/mish/controllers/DownloadController.kt
+++ src/main/kotlin/com/fim/prototype/mish/controllers/DownloadController.kt
@@ -2,6 +2,7 @@ package com.fim.prototype.mish.controllers
 
 import com.fim.prototype.mish.services.ModelService
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
@@ -10,6 +11,8 @@ open class DownloadController(
     open val modelService: ModelService
 ) {
 
+    //TODO should be removed after global security will be implemented..
+    @CrossOrigin(origins = ["http://localhost:8081"])
     @GetMapping("/download/{itemId}")
     fun downloadFile(@PathVariable itemId: String, response: HttpServletResponse) {
         val resource = modelService.getFileById(itemId)
