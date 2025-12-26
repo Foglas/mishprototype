b9fe2cbdcf8ca893c7bb52fd8963ea6f1fcf685d
Foglas <kubik.fogl@gmail.com>
2025-12-26 21:55:08 +0100

    cleanup

--- src/main/kotlin/com/fim/prototype/mish/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/fim/prototype/mish/security/config/SecurityConfig.kt
@@ -1,43 +1,16 @@
-import com.fim.prototype.mish.security.adapter.LdapUserAdapter
-import com.fim.prototype.mish.security.mapper.OidcUserMapper
-import jakarta.servlet.FilterChain
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.ObjectProvider
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
-import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(
-    private val oidcUserAdapterProvider: ObjectProvider<OidcUserMapper>,
-    private val ldapUserAdapterProvider: ObjectProvider<LdapUserAdapter>
-) {
-
-    private val logger = LoggerFactory.getLogger(SecurityConfig::class.java)
-
-    @Value("\${auth.provider}")
-    private lateinit var authProvider: String
-
-    @Bean
-    fun authenticationManager(): AuthenticationManager? =
-        when (authProvider.lowercase()) {
-            "ldap" -> ldapUserAdapterProvider.ifAvailable?.let { ProviderManager(listOf(it)) }
-            else -> null
-        }
+class SecurityConfig {
 
     @Bean
     @Order(1)
--- src/main/kotlin/com/fim/prototype/mish/security/mapper/UserMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.fim.prototype.mish.security.mapper
-
-import com.fim.prototype.mish.security.model.CurrentUser
-
-interface UserMapper<T> {
-
-    fun map(user: T): CurrentUser
-}
