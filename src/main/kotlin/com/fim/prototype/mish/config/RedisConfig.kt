1699e903973914f454ee4972cde39f07aefc738d
Foglas <kubik.fogl@gmail.com>
2026-02-20 00:58:49 +0100

    fix for production deployment

--- src/main/kotlin/com/fim/prototype/mish/config/RedisConfig.kt
+++ src/main/kotlin/com/fim/prototype/mish/config/RedisConfig.kt
@@ -1,17 +1,14 @@
 package com.fim.prototype.mish.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisClusterConfiguration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
@@ -21,12 +18,16 @@ class RedisConfig {
     @Bean
     fun redisConnectionFactory(properties: RedisProperties): RedisConnectionFactory {
         return if (!properties.cluster?.nodes.isNullOrEmpty()) {
+            val clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()
+                .build()
+
             val clusterConfig = RedisClusterConfiguration(properties.cluster!!.nodes)
             if (!properties.username.isNullOrEmpty() || !properties.password.isNullOrEmpty()) {
                 clusterConfig.setUsername(properties.username)       // <-- add this
                 clusterConfig.setPassword(RedisPassword.of(properties.password))
             }
-            LettuceConnectionFactory(clusterConfig)
+            LettuceConnectionFactory(clusterConfig, clientConfig)
         } else {
             val factory = LettuceConnectionFactory(
                 properties.host,
